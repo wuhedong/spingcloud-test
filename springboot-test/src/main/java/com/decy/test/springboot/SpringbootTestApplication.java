c5ca95e03f7522ac551b9c6e4060a46dcca1619d
wuhedong <846215693@qq.com>
2019-12-23 10:06:29 +0800

    add: spring test

---
+++ springboot-test/src/main/java/com/decy/test/springboot/SpringbootTestApplication.java
@@ -0,0 +1,13 @@
+package com.decy.test.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTestApplication.class, args);
+    }
+
+}
